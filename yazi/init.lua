00e799d7c30a80fe15965f8a41378a4c55791c99
cnhyk <nai.ying.cnhyk@gmail.com>
2026-01-17 05:28:59 +0800

    add bat and git themes

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,8 +1,10 @@
+-- full-border
 require("full-border"):setup({
 	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
 	type = ui.Border.ROUNDED,
 })
 
+-- git
 th.git = th.git or {}
 th.git.modified = ui.Style():fg("blue")
 th.git.deleted = ui.Style():fg("red"):bold()
@@ -10,4 +12,5 @@ th.git.modified_sign = "M"
 th.git.deleted_sign = "D"
 require("git"):setup()
 
+-- starship
 require("starship"):setup()
