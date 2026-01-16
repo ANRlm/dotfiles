e034448ee39e0c40289471bb081739818344155c
cnhyk <nai.ying.cnhyk@gmail.com>
2026-01-17 04:30:13 +0800

    add yazi-starship plugin

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,10 +1,13 @@
-require("full-border"):setup {
+require("full-border"):setup({
 	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
 	type = ui.Border.ROUNDED,
-}
+})
 
--- ~/.config/yazi/init.lua
 th.git = th.git or {}
+th.git.modified = ui.Style():fg("blue")
+th.git.deleted = ui.Style():fg("red"):bold()
 th.git.modified_sign = "M"
 th.git.deleted_sign = "D"
 require("git"):setup()
+
+require("starship"):setup()
