d40f5a5c0decd5536101e272d76e96fab3e6dd1d
cnhyk <nai.ying.cnhyk@gmail.com>
2026-01-22 05:53:48 +0800

    update ghostty karabiner neovide nvim restore.sh config

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -9,12 +9,29 @@ return {
   { import = "astrocommunity/colorscheme/rose-pine" },
   -- import/override with your plugins folder
 
+  -- {
+  --   "rose-pine/neovim",
+  --   name = "rose-pine",
+  --   opts = {
+  --     disable_background = true,
+  --     disable_float_background = true,
+  --   },
+  -- },
   {
     "rose-pine/neovim",
     name = "rose-pine",
-    opts = {
-      disable_background = true,
-      disable_float_background = true,
-    },
+    opts = function()
+      if vim.g.neovide then
+        return {
+          disable_background = false,
+          disable_float_background = false,
+        }
+      else
+        return {
+          disable_background = true,
+          disable_float_background = true,
+        }
+      end
+    end,
   },
 }
