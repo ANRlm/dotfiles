7c8d644c7ed9b7bdd3bd180b8c3c5417707e7db9
cnhyk <nai.ying.cnhyk@gmail.com>
2026-01-27 09:03:23 +0800

    update ghostty tmux restore

--- restore.sh
+++ restore.sh
@@ -4,47 +4,56 @@ set -Eeuo pipefail
 IFS=$'\n\t'
 
 OS="$(uname -s)"
+echo "Detected OS: $OS"
+
 DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
 CONFIG_DIR="$HOME/.config"
 
-echo "Detected OS: $OS"
-echo "Detected Dotfiles Dir: $DOTFILES_DIR"
-
 mkdir -p "$CONFIG_DIR"
 
+# Tmux restore
+mkdir -p "$CONFIG_DIR/tmux"
+mkdir -p "$CONFIG_DIR/tmux/plugins/catppuccin"
 git clone https://github.com/gpakosz/.tmux.git "$DOTFILES_DIR/oh-my-tmux"
-ln -sf "$DOTFILES_DIR/oh-my-tmux/.tmux.conf" "$DOTFILES_DIR/tmux/tmux.conf"
+git clone -b v2.1.3 https://github.com/catppuccin/tmux.git "$CONFIG_DIR/tmux/plugins/catppuccin/tmux"
+ln -sf "$DOTFILES_DIR/oh-my-tmux/.tmux.conf" "$CONFIG_DIR/tmux/tmux.conf"
+ln -sf "$DOTFILES_DIR/tmux/tmux.conf.local" "$CONFIG_DIR/tmux/tmux.conf.local"
 
+# Macos restore
 if [[ "$OS" == "Darwin" ]]; then
-    MACOS_APP_SUPPORT="$HOME/Library/Application Support"
-
-    for dir in aerospace bat btop fish ghostty nvim yazi tmux karabiner neovide mole git lazygit fastfetch go-musicfox alacritty; do
+    for dir in aerospace alacritty bat btop fastfetch fish ghostty git go-musicfox karabiner lazygit mole neovide nvim yazi; do
         rm -rf "$CONFIG_DIR/$dir"
         ln -s "$DOTFILES_DIR/$dir" "$CONFIG_DIR/$dir"
-
     done
 
-    ln -sfn "$DOTFILES_DIR/eza" "$MACOS_APP_SUPPORT/eza"
+    # Eza restore
+    ln -sfn "$DOTFILES_DIR/eza" "$HOME/Library/Application Support/eza"
 
-    echo "Disabling Gatekeeper..."
+    # Disable Gatekeeper
     sudo spctl --master-disable || true
 
-    echo "Restoring Homebrew packages..."
+    # Homebrew restore
     brew bundle
 
+# Linux restore
 elif [[ "$OS" == "Linux" ]]; then
-    for dir in bat btop lazygit eza yazi helix nvim fish tmux git alacritty; do
+    for dir in bat btop lazygit eza yazi helix nvim fish git alacritty; do
         rm -rf "$CONFIG_DIR/$dir"
         ln -s "$DOTFILES_DIR/$dir" "$CONFIG_DIR/$dir"
     done
 fi
 
+# Zsh restore
 ln -sf "$DOTFILES_DIR/zsh/.hushlogin" "$HOME/.hushlogin"
 ln -sf "$DOTFILES_DIR/zsh/zprofile"   "$HOME/.zprofile"
 ln -sf "$DOTFILES_DIR/zsh/zshrc"      "$HOME/.zshrc"
+# Starship restore
 ln -sf "$DOTFILES_DIR/starship/starship.toml" "$CONFIG_DIR/starship.toml"
-ln -sf "$DOTFILES_DIR/conda/.condarc"         "$HOME/.condarc"
-ln -sf "$DOTFILES_DIR/idea/.ideavimrc"        "$HOME/.ideavimrc"
-ln -sf "$DOTFILES_DIR/.gitconfig"       "$HOME/.gitconfig"
+# Conda restore
+ln -sf "$DOTFILES_DIR/conda/.condarc" "$HOME/.condarc"
+# Idea vim restore
+ln -sf "$DOTFILES_DIR/idea/.ideavimrc" "$HOME/.ideavimrc"
+# Git restore
+ln -sf "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
 
 echo "Dotfiles restored successfully!"
