bbb5a902e4ec7ecb80610624bffcd45924af4b37
cnhyk <nai.ying.cnhyk@gmail.com>
2026-01-29 16:32:12 +0800

    updat fzf

--- fish/config.fish
+++ fish/config.fish
@@ -154,11 +154,6 @@ if status is-interactive
 
     # FZF
     set -Ux FZF_DEFAULT_OPTS "\
-    --color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
-    --color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
-    --color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
-    --color=selected-bg:#45475A \
-    --color=border:#6C7086,label:#CDD6F4 \
     --height 75% \
     --layout=reverse \
     --border \
@@ -168,7 +163,7 @@ if status is-interactive
     fzf_configure_bindings --directory=\ct --history=\cr
     set -g fzf_preview_dir_cmd eza --all --color=always --icons --git --tree --level=2
     set -g fzf_preview_file_cmd bat --style=numbers --color=always --line-range :500
-    set -g fzf_diff_highlighter delta --paging=never --features="mellow-barbet" --syntax-theme="rose-pine"
+    set -g fzf_diff_highlighter delta --paging=never --features="mellow-barbet" --syntax-theme="Catppuccin Mocha"
     set -g fzf_history_time_format %d-%m-%y
 
     bind \cg ripgrep_search
