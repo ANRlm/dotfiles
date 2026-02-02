811ebaf41c69d27496bcc8738f96697557073fd7
cnhyk <nai.ying.cnhyk@gmail.com>
2026-02-02 23:29:00 +0800

    merge branches

--- restore.sh
+++ restore.sh
@@ -10,54 +10,53 @@ DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
 CONFIG_DIR="$HOME/.config"
 
 mkdir -p "$CONFIG_DIR"
-
-# Tmux restore
+# --------------------------------------------------
+# tmux
+# --------------------------------------------------
 mkdir -p "$CONFIG_DIR/tmux"
 mkdir -p "$CONFIG_DIR/tmux/plugins/catppuccin"
 if [ ! -d "$DOTFILES_DIR/oh-my-tmux" ]; then
-  git clone https://github.com/gpakosz/.tmux.git "$DOTFILES_DIR/oh-my-tmux"
+	git clone https://github.com/gpakosz/.tmux.git "$DOTFILES_DIR/oh-my-tmux"
 fi
 if [ ! -d "$CONFIG_DIR/tmux/plugins/catppuccin/tmux" ]; then
-  git clone -b v2.1.3 https://github.com/catppuccin/tmux.git "$CONFIG_DIR/tmux/plugins/catppuccin/tmux"
+	git clone -b v2.1.3 https://github.com/catppuccin/tmux.git "$CONFIG_DIR/tmux/plugins/catppuccin/tmux"
 fi
 ln -sf "$DOTFILES_DIR/oh-my-tmux/.tmux.conf" "$CONFIG_DIR/tmux/tmux.conf"
 ln -sf "$DOTFILES_DIR/tmux/tmux.conf.local" "$CONFIG_DIR/tmux/tmux.conf.local"
-
-# Macos restore
+# --------------------------------------------------
+# dotfiles
+# --------------------------------------------------
+for dir in aerospace alacritty bat btop eza fish ghostty git go-musicfox helix karabiner lazygit mole neovide nvim yazi; do
+	rm -rf "$CONFIG_DIR/$dir"
+	ln -s "$DOTFILES_DIR/$dir" "$CONFIG_DIR/$dir"
+done
+# --------------------------------------------------
+# macOS
+# --------------------------------------------------
 if [[ "$OS" == "Darwin" ]]; then
-    for dir in aerospace alacritty bat btop fish ghostty git go-musicfox karabiner lazygit mole neovide nvim yazi; do
-        rm -rf "$CONFIG_DIR/$dir"
-        ln -s "$DOTFILES_DIR/$dir" "$CONFIG_DIR/$dir"
-    done
+	# eza
+	ln -sfn "$DOTFILES_DIR/eza" "$HOME/Library/Application Support/eza"
 
-    # Eza restore
-    ln -sfn "$DOTFILES_DIR/eza" "$HOME/Library/Application Support/eza"
+	# disable Gatekeeper
+	sudo spctl --master-disable || true
 
-    # Disable Gatekeeper
-    sudo spctl --master-disable || true
-
-    # Homebrew restore
-    brew bundle
-
-# Linux restore
-elif [[ "$OS" == "Linux" ]]; then
-    for dir in bat btop lazygit eza yazi helix nvim fish git alacritty; do
-        rm -rf "$CONFIG_DIR/$dir"
-        ln -s "$DOTFILES_DIR/$dir" "$CONFIG_DIR/$dir"
-    done
+	# homebrew
+	brew bundle
 fi
-
-# Zsh restore
+# --------------------------------------------------
+# others
+# --------------------------------------------------
+# zsh
 ln -sf "$DOTFILES_DIR/zsh/.hushlogin" "$HOME/.hushlogin"
-ln -sf "$DOTFILES_DIR/zsh/zprofile"   "$HOME/.zprofile"
-ln -sf "$DOTFILES_DIR/zsh/zshrc"      "$HOME/.zshrc"
-# Starship restore
+ln -sf "$DOTFILES_DIR/zsh/zprofile" "$HOME/.zprofile"
+ln -sf "$DOTFILES_DIR/zsh/zshrc" "$HOME/.zshrc"
+# starship
 ln -sf "$DOTFILES_DIR/starship/starship.toml" "$CONFIG_DIR/starship.toml"
-# Conda restore
+# conda
 ln -sf "$DOTFILES_DIR/conda/.condarc" "$HOME/.condarc"
-# Idea vim restore
+# idea vim
 ln -sf "$DOTFILES_DIR/idea/.ideavimrc" "$HOME/.ideavimrc"
-# Git restore
+# git
 ln -sf "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
 
 echo "Dotfiles restored successfully!"
